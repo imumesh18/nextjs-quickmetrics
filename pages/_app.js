4768914823331948356806cced5d0d794b47828f
Umesh Yadav <umesh.yadav@go-jek.com>
2020-11-09 21:25:03 +0530

    Add Next.js quick metrics example
    Signed-off-by: Umesh Yadav <umesh.yadav@go-jek.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const sendMetric = ({ name, value }) => {
+  const url = `https://qckm.io?m=${name}&v=${value}&k=${process.env.NEXT_PUBLIC_QUICK_METRICS_API_KEY}`;
+
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url);
+  } else {
+    fetch(url, { method: "POST", keepalive: true });
+  }
+};
+
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case "FCP":
+      sendMetric(metric);
+      break;
+    case "LCP":
+      sendMetric(metric);
+      break;
+    case "CLS":
+      sendMetric(metric);
+      break;
+    case "FID":
+      sendMetric(metric);
+      break;
+    case "TTFB":
+      sendMetric(metric);
+      break;
+    default:
+      break;
+  }
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
